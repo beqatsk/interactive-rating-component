6dc9696c7c232fbe988757247c46fba2fcc81520
beqa <beqa.tskhvediani@gmail.com>
2024-02-29 10:32:06 +0400

    add raiting paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ function App() {
       <div className="rating-card">
         <img src={Star} alt="star" />
         <h1>How did we do?</h1>
+        <p>
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
       </div>
     </>
   );
