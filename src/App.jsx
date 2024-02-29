e67fdf74ccc78f9a1ae1036cd5aa35c1992a290b
beqa <beqa.tskhvediani@gmail.com>
2024-02-29 22:24:28 +0400

    add result component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Rate from "./components/Rate/Rate";
+import Result from "./components/Result/Result";
 
 function App() {
   return (
     <>
       <Rate />
+      <Result />
     </>
   );
 }
--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,1 +1,16 @@
 import "./Result.css";
+import Payment from "/images/payment.png";
+
+export default function Result() {
+  return (
+    <div className="result-card">
+      <img src={Payment} alt="payment" className="payment" />
+      <div className="selected">You selected 4 out of 5</div>
+      <h3>Thank you!</h3>
+      <p className="result-paragraph">
+        We appreciate you taking the time to give a rating. If you ever need
+        more support, don’t hesitate to get in touch!
+      </p>
+    </div>
+  );
+}
